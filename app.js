a2fea28fe7579b8e7802f0d7bb0975e140ca5f4e
kotakbulat <hokilimpah@gmail.com>
2024-01-04 19:20:01 +0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
-//console.log("Hello world");
-//import chalk from chalk;
-const validator = require("validator");
-const chalk = require("chalk");
-const {simpanContact, tulisPertanyaan} = require("./contacts");
+const yargs = require("yargs");
+const { simpanContact } = require("./contacts");
+
+//console.log(process.argv);
+
+yargs.command({
+    command: "add",
+    describe: "Menambahkan contact baru",
+    builder: {
+        nama: {
+            describe: "Nama Lengkap",
+            demandOption: true,
+            type: "string",
+        },
+        email: {
+            describe: "Email",
+            demandOption: false,
+            type: "string",
+        },
+        nohp: {
+            describe: "No HP",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler(argv) {
+        //console.log("Menambahkan contact baru", argv);
+        //  const contact = {
+        //     nama: argv.nama,
+        //     email: argv.email,
+        //     nohp: argv.nohp,
+        simpanContact(argv.nama, argv.email, argv.nohp);
+        },
+        // console.log(contact);
+    },      
+).demandCommand();
+
+yargs.parse();
 
 
 
-//test = validator.isEmail("hoki@aksjbd.sk");
-//test = validator.isMobilePhone("+1289699868863", "id-ID");
-//console.log(test);
+
+
+
+
+
+
+
+// const chalk = require("chalk");
+// const {simpanContact, tulisPertanyaan} = require("./contacts");
 
 // nama = 'Hoki';
 // pesan = chalk`lorem ipsum dolor sit amet {bgRed.bold.italic consectetur} adipisicing elit, nama saya : ${nama}`;
 // console.log(pesan);
-const main  = async () => {
-    const nama = await tulisPertanyaan("Masukan nama anda : ");
-    const email = await tulisPertanyaan("Masukan email anda : ");
-    const nohp = await tulisPertanyaan("Masukan nohp anda : ");
-    simpanContact(nama, email, nohp);
-}
+// const main  = async () => {
+//     const nama = await tulisPertanyaan("Masukan nama anda : ");
+//     const email = await tulisPertanyaan("Masukan email anda : ");
+//     const nohp = await tulisPertanyaan("Masukan nohp anda : ");
+//     simpanContact(nama, email, nohp);
+// }
 
-main();
+// main();
 
--- contacts.js
+++ contacts.js
@@ -9,6 +9,31 @@ simpanContact = (nama, email, nohp) => {
     const contact = {nama, email, nohp};
     const file = fs.readFileSync("data/contacts.json", "utf-8");
     const contacts = JSON.parse(file);
+    //cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if(duplikat){
+        console.log("Contact sudah terdaftar, gunakan nama lain!");
+        rl.close();
+        return false;
+    }
+    //cek email
+    if(!validator.isEmail(email)){
+        const email = contacts.find((contact) => contact.email === email);
+        if(email){
+            console.log("Email sudah terdaftar, gunakan email lain!");
+            rl.close();
+            return false;
+        }
+    } 
+    //cek nohp
+    if(!validator.isMobilePhone(nohp, "id-ID")){
+        const nohp = contacts.find((contact) => contact.nohp === nohp);
+        if(nohp){
+            console.log("No HP sudah terdaftar, gunakan nohp lain!");
+            rl.close();
+            return false;
+        }
+    }
     contacts.push(contact);
     fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
     console.log("Data berhasil ditambahkan");
@@ -22,12 +47,6 @@ if(!fs.existsSync(dirPath)){
 if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, "[]", "utf-8");
 }
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama);
-        });
-    });
-}
 
-module.exports = {simpanContact, tulisPertanyaan};
+
+module.exports = {simpanContact};
